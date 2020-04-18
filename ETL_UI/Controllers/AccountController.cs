b7d348476c23a421532d129022d92b3ff843b045
Obuli <obulisundaram1991@gmail.com>
2020-04-18 12:05:31 +0000

    Login Validate

--- ETL_UI/Controllers/AccountController.cs
+++ ETL_UI/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ETL_UI.Models;
 
 namespace ETL_UI.Controllers
 {
@@ -11,6 +12,11 @@ namespace ETL_UI.Controllers
         public IActionResult Login()
         {
             return View();
+        }
+
+        public IActionResult Validate(Account _account)
+        {
+            return RedirectToAction("Index","Home");
         }
     }
 }
